690383e9a0f06726da20b50f6c7d1ea41c414df4
Michael <michaelcruz552@gmail.com>
2026-07-15 12:09:05 +0800

    feat: initial commit of project frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import LoginForm from "./components/LoginForm"
+
+function App() {
+  return <>
+    <div className="row justify-content-center">
+      <div className="col-4 p-5 mt-5 border rounded-3 shadow">
+        <LoginForm />
+      </div>
+    </div>1
+  </>
+}
+export default App
