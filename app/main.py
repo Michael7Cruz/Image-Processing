b100efe2a6b1c4e250f6184f5e77ff6ca4f0ffac
Michael <michaelcruz552@gmail.com>
2026-07-15 12:22:30 +0800

    feat: add CORS middleware with the frontend origin

--- app/main.py
+++ app/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI
 from app.router import image, user
 from app.core.config import get_settings
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.include_router(user.router)
 app.include_router(image.router)
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 async def root():
